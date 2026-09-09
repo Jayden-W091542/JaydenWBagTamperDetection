2191bca572b2d17191607b8a1ed55c65c6ad5571
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2026-09-09 14:37:21 +1200

    Update script.js
    HAN teacher comments added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
- function updateStatus(n) {
+//HAN comment - what does this code do?
+function updateStatus(n) {
     const checkbox = document.getElementById('toggleControl' + n);
     const status = document.getElementById('toggleStatus' + n);
     if (checkbox.checked) {
@@ -9,3 +10,6 @@
       status.classList.remove('on');
     }
   }
+
+//HAN comment - I would also note that our example code has about 
+//30 lines of code that will help with being a web server that I was expecting to see?
